59319c2806bbdf2181c1f39b547f0f55055ecc93
kathejim <147934904+kathejim@users.noreply.github.com>
2024-08-31 18:42:40 -0500

    Declare global variables: form, input, select and span elements.

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create global variables
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const bdate = document.querySelector("#bdate");
+const language = document.querySelector("#language-select");
+const pass = document.querySelector("#pass");
+const confirmPass = document.querySelector("#confirm-pass");
+const nameError = document.querySelector("#email .error");
+
+
