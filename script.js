46a726de6ca332297c25a6da1b2a70ceae948391
kathejim <147934904+kathejim@users.noreply.github.com>
2024-08-31 18:56:37 -0500

    Add Event Listener for submit button and prevent default.

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 // Create global variables
 const form = document.querySelector("form");
-const name = document.querySelector("#name");
+const username = document.querySelector("#name");
 const email = document.querySelector("#email");
 const bdate = document.querySelector("#bdate");
 const language = document.querySelector("#language-select");
 const pass = document.querySelector("#pass");
 const confirmPass = document.querySelector("#confirm-pass");
-const nameError = document.querySelector("#email .error");
+const usernameError = document.querySelector("#email .error");
 
+username.addEventListener("input", (event) => {
+    // Each time the user types something, we check if the
+    // form fields are valid.
 
+    if (username.validity.valid) {
+        username.textContent = "";
+        username.className = "error";
+    }
+    else {
+        showError();
+    }
+});
+
+form.addEventListener("submit", (event) => {
+
+    if (!username.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+});
