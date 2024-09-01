4cba88d6f2268185c0578a8d01052d6429adaaa8
kathejim <147934904+kathejim@users.noreply.github.com>
2024-09-01 10:12:25 -0500

    Create a base code with customized functions to validate all fields.
    Create an array with constants for every field. Create a CheckValidity function to check the validity of each field according to the HTML validation. Create a ShowError function that creates an Error message constant for each field and then checks if each field complies with all the requirements in HTML.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Create global variables
 const form = document.querySelector("form");
+
 const username = document.querySelector("#name");
 const email = document.querySelector("#email");
 const bdate = document.querySelector("#bdate");
@@ -7,8 +8,50 @@ const language = document.querySelector("#language-select");
 const pass = document.querySelector("#pass");
 const confirmPass = document.querySelector("#confirm-pass");
 
-const usernameError = document.querySelector("#name + span.error");
+//const fieldNames = ["#name", "#email", "#bdate", "#language-select", "#pass", "#confirm-pass"];
+const field = [username, email, bdate, language, pass, confirmPass];
 
+//const usernameError = document.querySelector("#name + span.error");
+
+form.addEventListener("submit", (event) => {
+
+    const validatedFields = 0;
+
+    for (i = 0; i < 6; i++) {
+
+        checkValidity(field[i]);
+        if (checkValidity === True) {
+            validatedFields =+ 1;
+        };
+
+    if (validatedFields != 6) {
+        event.preventDefault;
+    }
+}});
+
+function checkValidity(field) {
+
+    if (!field.validity.valid) {
+        showError(field);
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function showError(field) {
+
+    const fieldError = document.querySelector(`#${field} + span.error`);
+
+    if (field.validity.valueMissing) {
+        fieldError.textContent = "You must fill this field.";
+    } else if (field.validity.tooShort) {
+        fieldError.textContent = `This value should be at least ${field.minLength} characters.`;
+    }
+}
+
+//REAL-TIME VALIDATION
 // username.addEventListener("input", (event) => {
 //     // Each time the user types something, we check if the
 //     // form fields are valid.
@@ -19,19 +62,3 @@ const usernameError = document.querySelector("#name + span.error");
 //         showError();
 //     }
 // });
-
-form.addEventListener("submit", (event) => {
-
-    if (!username.validity.valid) {
-        showError();
-        event.preventDefault();
-    }
-});
-
-function showError() {
-    if (username.validity.valueMissing) {
-        usernameError.textContent = "You need to enter an username.";
-    } else if (username.validity.tooShort) {
-        usernameError.textContent = `Username should be at least ${username.minLength} characters.`;
-    }
-}
